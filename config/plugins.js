e73692a2c1aead3bdb79236ca3a2d93ca5be43ae
xl <1293358059@qq.com>
2023-12-20 11:29:55 +0800

    fix: fix

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://strapi-demo-flame.vercel.app",
-      apiToken: "9BF5N3SOyth9BHthew7Fb15b",
-      appFilter: "your-app-name-on-vercel",
+        "https://api.vercel.com/v1/integrations/deploy/prj_wbn5vw84ikGlL34Tued4DUYkZTzJ",
+      apiToken: "8ab6f316a85414071a78a2e726bef23b9962306779dfe4bb90447055e48d3e65ff5b7dd9357ac4ca1c5fda8ed84687588c2f66c238f348692d827ffcc5400a267639838c5ae276c1f1309c5ca1c744534928d251ab42535baee27b49135953404dc9090f8d3f912e2e2fea7002e12f69914ca7fe42a7569fc903a80b656c1734",
+      appFilter: "strapi-demo",
       teamFilter: "your-team-id-on-vercel",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
